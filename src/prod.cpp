16907a830a3bc700fdc2dbc8b4ec09a05a87d34d
cs293-ahilkhancse <ahilkhancse@cse.iitb.ac.in>
2024-10-19 03:44:28 +0530

    added support for negative number as well as zero

--- src/prod.cpp
+++ src/prod.cpp
@@ -16,10 +16,23 @@ double compute_log_sum(int* a, int size, int num_threads) {
 
     #pragma omp parallel for default(shared) reduction(+:log_sum) num_threads(num_threads)
     for (int i = 0; i < size; i++) {
-        log_sum += log(a[i]);
+        log_sum += log(abs(a[i]));
     }
 
     return log_sum;
+}
+
+// Function to find number of negative numbers in parallel
+pair<int, int> count_negative_and_zero(int* a, int size, int num_threads) {
+    int negative_count = 0, zero_count = 0;
+
+    #pragma omp parallel for private(negative_count, zero_count) reduction(+:negative_count, zero_count) num_threads(num_threads)
+    for (int i = 0; i < size; i++) {
+        if(a[i] < 0 )negative_count++;
+        else if(a[i] == 0)zero_count++;
+    }
+
+    return {negative_count, zero_count};
 }
 
 // Function to compute the product using the sum of logarithms
@@ -38,11 +51,24 @@ int main(int argc, char *argv[]) {
     // Initialize array
     initialize_array(a, ARR_SIZE, 2);
 
+    pair<int, int> neg_and_zero_count = count_negative_and_zero(a, ARR_SIZE, num_threads);
+    if (neg_and_zero_count.second > 0)
+    {
+        // Print the result
+        cout << "Product=" << 0 << endl;
+        return 0;
+    }
+
     // Compute the log sum in parallel
     double log_sum = compute_log_sum(a, ARR_SIZE, num_threads);
 
     // Compute the product using the log sum
     double prod = compute_product(log_sum);
+
+    if(neg_and_zero_count.first%2 == 1)
+    {
+        prod = -prod;
+    }
 
     // Print the result
     cout << "Product=" << prod << endl;
