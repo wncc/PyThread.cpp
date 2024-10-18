7f8c090a550d3c7d04aaef1103514de58c2586ec
cs293-ahilkhancse <ahilkhancse@cse.iitb.ac.in>
2024-10-18 22:40:06 +0530

    montecarlo same random number issue resolved

--- src/montecarlo.cpp
+++ src/montecarlo.cpp
@@ -2,16 +2,25 @@
 #include <iostream>
 #include <omp.h>
 #include <cstdlib>
+#include <chrono>
 using namespace std;
+
+unsigned long long getCurrentTimeInMilliseconds() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
 
 void montecarlo(int n, int num_threads) {
     int pCircle = 0, pSquare = 0;
     double x, y, d;
     int i;
+    unsigned long long seed = getCurrentTimeInMilliseconds();
+    srand(seed); // Seeding with a modified value
 
     // Parallelize the loop using OpenMP
     #pragma omp parallel for private(x, y, d, i) reduction(+:pCircle, pSquare) num_threads(num_threads)
     for(i = 0; i < n; i++) {
+
         // Generate random points between 0 and 1
         x = (double)rand() / RAND_MAX;
         y = (double)rand() / RAND_MAX;
